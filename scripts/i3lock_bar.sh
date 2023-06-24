1329d67481a548fc9e67cfe5112bcb2029150058
zioer <next4nextjob@gmail.com>
2023-06-24 21:55:04 +0800

    feat: 🎸 增加i3lock的两个锁屏脚本
    使用i3lock-color实现的锁屏脚本替代了 i3lock-fancy

---
+++ scripts/i3lock_bar.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# RGBA color
+BLANK='#00000000'
+CLEAR='#ffffff22'
+DEFAULT='#FF5722FF'
+TEXT='#DCEDC8ee'
+WRONG='#D50000FF'
+VERIFYING='#4CAF50FF'
+blur=6
+
+i3lock                      \
+--blur $blur                \
+--bar-indicator             \
+--bar-pos y+h               \
+--bar-direction 1           \
+--bar-max-height 50         \
+--bar-base-width 50         \
+--bar-color $BLANK          \
+--keyhl-color $VERIFYING    \
+--bar-periodic-step 50      \
+--bar-step 50               \
+--redraw-thread             \
+\
+--clock \
+--force-clock \
+--time-pos x+5:y+h-80       \
+--time-color $TEXT          \
+--date-pos tx:ty+15         \
+--date-color $TEXT          \
+--date-align 1              \
+--time-align 1              \
+--ringver-color $VERIFYING  \
+--ringwrong-color $WRONG    \
+--status-pos x+5:y+h-16     \
+--verif-align 1             \
+--wrong-align 1             \
+--verif-color $VERIFYING    \
+--wrong-color $WRONG        \
+--modif-pos -50:-50
---
+++ scripts/i3lock_round.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+# RGBA color
+BLANK='#00000000'
+CLEAR='#ffffff22'
+DEFAULT='#FF5722FF'
+TEXT='#DCEDC8ee'
+WRONG='#D50000FF'
+VERIFYING='#4CAF50FF'
+width=12
+radius=120
+blur=6
+
+i3lock \
+--insidever-color=$CLEAR     \
+--ringver-color=$VERIFYING   \
+\
+--insidewrong-color=$CLEAR   \
+--ringwrong-color=$WRONG     \
+\
+--inside-color=$BLANK        \
+--radius=$radius             \
+--ring-color=$DEFAULT        \
+--ring-width=${width}        \
+--line-color=$BLANK          \
+--separator-color=$DEFAULT   \
+\
+--verif-color=$TEXT          \
+--wrong-color=$TEXT          \
+--time-color=$TEXT           \
+--date-color=$TEXT           \
+--layout-color=$TEXT         \
+--keyhl-color=$VERIFYING     \
+--bshl-color=$WRONG          \
+\
+--screen 1                   \
+--blur ${blur}               \
+--clock                      \
+--indicator                  \
+--time-str="%H:%M:%S"        \
+--date-str="%A, %Y-%m-%d"    \
+--keylayout 1                \
