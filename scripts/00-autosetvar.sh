bf6f325cbf4b1431f3fcc4323b7c9949649cb6e9
zioer <next4nextjob@gmail.com>
2023-06-28 08:48:56 +0800

    fix: 🐛 修改common/var.ini 为 templates/var.ini
    由于var.ini经常修改,于是便将其作为templates自动生成.var.ini文件

--- scripts/00-autosetvar.sh
+++ scripts/00-autosetvar.sh
@@ -2,19 +2,20 @@
 
 # 本脚本用于自动修改 var.ini 配置文件中变量
 
-config_file=$POLYBAR_HOME/common/var.ini
+config_file=$POLYBAR_HOME/template/var.ini
+output_file=$POLYBAR_HOME/.var.ini
 
 function set_cpu_path() { #设置CPU温度监控路径
     value=`ls /sys/devices/platform/coretemp.0/hwmon/hwmon*/temp1_input|head -1`
-    sed -i -E "s|^(hwmon_path =).*|\1 ${value}|g" $config_file
+    sed -i -E "s|^(hwmon_path =).*|\1 ${value}|g" $output_file
 }
 
 function set_network_interface() { #设置网络接口类型有线或无线
     value=`ip route|awk '/^default/{ print $5 }'|head -1`
-    sed -i -E "s|^(network_interface =).*|\1 ${value}|g" $config_file
+    sed -i -E "s|^(network_interface =).*|\1 ${value}|g" $output_file
 }
 
 ################################
-
+cp $config_file $output_file
 set_cpu_path
 set_network_interface
